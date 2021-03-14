1e527f8fdc290de6fb6fe6e254d1e7bfd8abd5bf
Guerder Vieira <g7.vieira@gmail.com>
2021-03-14 18:33:10 -0300

    extracted grid component

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/providers/products_provider.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/widgets/product_grid_widget.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<ProductsProvider>(context);
-    final products = productsProvider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, index) {
-          return ProductItem(products[index]);
-        },
-      ),
+      body: ProductGridWidget(),
     );
   }
 }
---
+++ lib/widgets/product_grid_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products_provider.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductGridWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final products = productsProvider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, index) {
+        return ProductItem(products[index]);
+      },
+    );
+  }
+}
