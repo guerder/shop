662d41345a2044392d1b044fa133c28c24e11d59
Guerder Vieira <g7.vieira@gmail.com>
2021-03-14 20:03:59 -0300

    implemented filtering of favorite products

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/enums/filter_options.dart';
 import 'package:shop/widgets/product_grid_widget.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  bool _showFavoriteOnly = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha loja'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorite) {
+                  _showFavoriteOnly = true;
+                } else {
+                  _showFavoriteOnly = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductGridWidget(),
+      body: ProductGridWidget(_showFavoriteOnly),
     );
   }
 }
