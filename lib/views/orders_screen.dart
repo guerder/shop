f17459696cc0d02f72743a588ae9b44039ce6e64
Guerder Vieira <g7.vieira@gmail.com>
2021-03-15 16:36:17 -0300

    finished section #8: implemented display of the list of orders placed

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/app_drawer.dart';
+import 'package:shop/widgets/order_widget.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final orders = Provider.of<Orders>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
       ),
       drawer: AppDrawer(),
+      body: ListView.builder(
+          itemCount: orders.itemsCount,
+          itemBuilder: (ctx, i) {
+            return OrderWidget(orders.items[i]);
+          }),
     );
   }
 }
