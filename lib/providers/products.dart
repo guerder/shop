4c527219bcf7d01a2a97c5dfaa102ae2574d92b7
Guerder Vieira <g7.vieira@gmail.com>
2021-03-20 20:06:13 -0300

    finished section #9: added product deletion action

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -39,4 +39,12 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    final index = _items.indexWhere((prod) => prod.id == id);
+    if (index >= 0) {
+      _items.removeWhere((product) => product.id == id);
+      notifyListeners();
+    }
+  }
 }
