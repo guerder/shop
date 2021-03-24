94bb6876a2b7ac0e268eecb2b7bba8a5a71db5be
Guerder Vieira <g7.vieira@gmail.com>
2021-03-24 20:49:38 -0300

    implemented auto logoff after session time expired

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String _userId;
   String _token;
   DateTime _expiryDate;
+  Timer _logoutTimer;
 
   bool get isAuth {
     return token != null;
@@ -52,6 +54,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseBody['expiresIn']),
         ),
       );
+      _autoLogout();
       notifyListeners();
     }
 
@@ -70,6 +73,20 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+
+    if (_logoutTimer != null) {
+      _logoutTimer.cancel();
+      _logoutTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_logoutTimer != null) {
+      _logoutTimer.cancel();
+    }
+
+    final timeToLogout = _expiryDate.difference(DateTime.now()).inSeconds;
+    _logoutTimer = Timer(Duration(seconds: timeToLogout), logout);
   }
 }
