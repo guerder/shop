907a37770b4496e53ffbc04a272c2d0c7c9213c8
Guerder Vieira <g7.vieira@gmail.com>
2021-04-06 21:11:06 -0300

    added animations on the product details screen

--- lib/widgets/product_grid_item_widget.dart
+++ lib/widgets/product_grid_item_widget.dart
@@ -23,9 +23,13 @@ class ProductGridItemWidget extends StatelessWidget {
               arguments: product,
             );
           },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
